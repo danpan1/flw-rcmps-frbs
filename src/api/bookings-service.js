1f313397f611fa382b2992cfe07b7479bbce93ef
Pankrashin Danil <danpan@yandex.ru>
2018-09-11 17:26:06 +0300

    Firebase

---
+++ src/api/bookings-service.js
@@ -0,0 +1,43 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyAZZRGFnOciwLZsSMjBlilnxfIlPcXXtc4",
+  // authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://flw-rcmps-frbs.firebaseio.com",
+  // storageBucket: "bucket.appspot.com"
+};
+
+class BookingsService {
+  constructor(){
+    firebase.initializeApp(config);
+    this.database = firebase.database();
+    this.ref = 'bookings';
+  }
+
+  get bookings(){
+    return new Promise((resolve, reject)=>{
+      let items = [];
+      const ref = this.database.ref(this.ref);
+      ref.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          const childKey = childSnapshot.key;
+          const childData = childSnapshot.val();
+          const item = {id: childKey, ...childData};
+          items.push(item);
+        });
+        resolve(items)
+      });
+    })
+  }
+
+  setBooking(userId, name, email, date){
+    this.database.ref(this.ref + '/' + userId).set({
+      username: name,
+      email: email,
+      date : date
+    });
+  }
+}
+export default new BookingsService();
+
+
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,49 +1,19 @@
 // @flow
 
 import React, { Component } from 'react';
-import firebase from 'firebase';
 import logo from '../assets/logo.svg';
 import './App.css';
+import BookingsService from "../api/bookings-service";
 
 
 
-var config = {
-  apiKey: "AIzaSyAZZRGFnOciwLZsSMjBlilnxfIlPcXXtc4",
-  // authDomain: "projectId.firebaseapp.com",
-  databaseURL: "https://flw-rcmps-frbs.firebaseio.com",
-  // storageBucket: "bucket.appspot.com"
-};
-
-
-
-
-firebase.initializeApp(config);
-var database = firebase.database();
-function writeUserData(userId, name, email, imageUrl) {
-  database.ref('rules/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
-
-
-
-let bookings = []
-function getII(){
-   const rulesRef = database.ref('rules');
-  rulesRef.once('value', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var childKey = childSnapshot.key;
-      var childData = childSnapshot.val();
-      const item = {id: childKey, ...childData}
-      bookings.push(item);
-      console.log('bookings', bookings);
-    });
-  });
-}
-getII()
 class HomePage extends Component {
+  componentDidMount() {
+    BookingsService.bookings.then(i=>{
+      console.log(i)
+    })
+  }
+
   render() {
     return (
       <div className="App">
